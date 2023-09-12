69d0729c015ab991f4e0ffc7e33c8df18ded6f0e
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-09-12 18:25:06 +0400

    add routing / 35 chapter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeeUps";
+import NewMeetUpPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<AllMeetupsPage />}></Route>
+      <Route path="/NewMeetUpPage" element={<NewMeetUpPage />}></Route>
+      <Route path="/FavoritesPage" element={<FavoritesPage />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/NewMeetup.jsx
@@ -0,0 +1,5 @@
+function NewMeetUpPage() {
+    return <div>New Meet up Page</div>;
+  }
+  
+  export default NewMeetUpPage;
