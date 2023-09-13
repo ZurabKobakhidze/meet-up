c6560bb3dd975830888452ce7c5bc13791695030
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-09-13 20:02:04 +0400

    49 chapter

--- src/store/favorites-context.jsx
+++ src/store/favorites-context.jsx
@@ -8,6 +8,22 @@ const FavoritesContext = createContext({
 function FavoritesContextProvider(props) {
   const [userfavorites, setUserFavorites] = useState([]);
 
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userfavorites.some(meetup => meetup.id === meetupId);
+  }
+
   const context = {
     favorites: userfavorites,
     totalFavorites: userfavorites.length,
