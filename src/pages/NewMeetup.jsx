848c4378bb66b3acef4150b4599531ffef0b5798
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-09-13 17:18:39 +0400

    44 chapter

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,14 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetUpPage() {
-function addMeetuphandler(meetupData) {
-  
-}
+  function addMeetuphandler(meetupData) {
+    fetch(
+      "https://react-getting-started-fab10-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method:  'Post' , 
+        body: JSON.stringify(meetupData),
+        headers: {
+          'content-Type' : 'application/json'
+        }
+      }
+    );
+  }
 
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm onAddMeetup={addMeetuphandler}/>
+      <NewMeetupForm onAddMeetup={addMeetuphandler} />
     </section>
   );
 }
